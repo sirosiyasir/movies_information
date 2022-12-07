7c188b52583f8fd0b29efef3933810d3aa712ded
Yasir <yasirse1223@hotmail.com>
2022-12-07 19:33:38 +0300

    add more css about homepage

--- app.js
+++ app.js
@@ -7,76 +7,73 @@ const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const https = require("https")
 
-
 const app = express()
-app.set('view engine', 'ejs');
+
+app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({ extended: true }))
 // public klasörümüzü dahil ediyoruz
 app.use(express.static("public"))
 
-app.get("/", function(req,res) {
-    /* const movieName = req.body.movieName
-    const url = "https://www.omdbapi.com/?apikey=86f9dde7&t="+movieName;
-      https.get(url, function(response) {
-        console.log(response.statusCode);
+app.get("/", function (req, res) {
+  /* const movieName = req.body.movieName
+  const url = "https://www.omdbapi.com/?apikey=86f9dde7&t="+movieName;
+    https.get(url, function(response) {
+      console.log(response.statusCode);
+      response.on("data", function(data) {
+          const movieData = JSON.parse(data)
+          console.log(movieData);
+      })
+    }) 
+  res.send("server is running") */
 
-        response.on("data", function(data) {
-            const movieData = JSON.parse(data)
-            console.log(movieData);
-        })
-      }) 
-    res.send("server is running") */
-  
-    res.render("home")
+  res.render("home")
 })
 
-app.post("/", function(req,res) {
-    const movieName = req.body.movieName
-    const url = "https://www.omdbapi.com/?apikey=86f9dde7&t="+movieName;
-      https.get(url, function(response) {
-        /* eğer app.post'ta değilde app.get'te yapsaydık normal bir apı alma işlemi çalışırdı ama
+app.post("/", function (req, res) {
+  const movieName = req.body.movieName
+  const url = "https://www.omdbapi.com/?apikey=86f9dde7&t=" + movieName
+  https.get(url, function (response) {
+    /* eğer app.post'ta değilde app.get'te yapsaydık normal bir apı alma işlemi çalışırdı ama
         post'ta aldığımız için boş bir dizi oluşturup gelen dataları önce onun içine atmalıyız daha sonra
         hepsi yüklenince "end" yöntemiyle de yansıtmalıyız (yoksa console'da "unexpected end of JSON input" hatası alınıyor)*/
-        let stockData = '';
-        response.on("data", function(data) {
-            stockData += data
+    let stockData = ""
+    response.on("data", function (data) {
+      stockData += data
+    })
+    response.on("end", function () {
+      // JSON.parser() metoduyla data'yı açıyoruz
+      const moviesData = JSON.parse(stockData)
+      const movieTitle = moviesData.Title
+      const moviePlot = moviesData.Plot
+      const moviePoster = moviesData.Poster
+      const movieImbd = moviesData.imdbRating
+      const movieActors = moviesData.Actors
+      const movieYear = moviesData.Year
+      const movieGenre = moviesData.Genre
+      const movieLanguage = moviesData.Language
+      const movieAwards = moviesData.Awards
+      const movieDirector = moviesData.Director
+      // buradaki moviesData.Response , apı'deki "Response"dir ve True'ya eşittir. console.log(moviesData)'yaparak görüntüleyebiliriz
+      if (moviesData.Response === "True") {
+        res.render("movies", {
+          movieTitle: movieTitle,
+          moviePlot: moviePlot,
+          moviePoster: moviePoster,
+          movieImbd: movieImbd,
+          movieActors: movieActors,
+          movieYear: movieYear,
+          movieGenre: movieGenre,
+          movieLanguage: movieLanguage,
+          movieAwards: movieAwards,
+          movieDirector: movieDirector,
         })
-        response.on("end", function() {
-          // JSON.parser() metoduyla data'yı açıyoruz
-            const moviesData = JSON.parse(stockData)
-            const movieTitle = moviesData.Title
-            const moviePlot = moviesData.Plot
-            const moviePoster = moviesData.Poster
-            const movieImbd = moviesData.imdbRating
-            const movieActors = moviesData.Actors
-            const movieYear = moviesData.Year
-            const movieGenre = moviesData.Genre
-            const movieLanguage = moviesData.Language
-            const movieAwards = moviesData.Awards
-            const movieDirector = moviesData.Director
-            // buradaki moviesData.Response , apı'deki "Response"dir ve True'ya eşittir. console.log(moviesData)'yaparak görüntüleyebiliriz
-            if(moviesData.Response === "True") { 
-              res.render("movies", {
-                movieTitle: movieTitle,
-                moviePlot: moviePlot,
-                moviePoster: moviePoster,
-                movieImbd: movieImbd,
-                movieActors: movieActors,
-                movieYear: movieYear,
-                movieGenre: movieGenre,
-                movieLanguage: movieLanguage,
-                movieAwards: movieAwards,
-                movieDirector: movieDirector
-              })
-            } else {
-              res.render("failure", {movieName:movieName})
-            }
-            
-        })
-      }) 
+      } else {
+        res.render("failure", { movieName: movieName })
+      }
+    })
+  })
 })
 
-
-app.listen(process.env.PORT || 3000, function(req,res) {
-    console.log("server is running on localhost:3000");
+app.listen(process.env.PORT || 3000, function (req, res) {
+  console.log("server is running on localhost:3000")
 })
--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,29 +1,27 @@
+const menu = document.getElementById("menu-title")
+const menuLinks = document.getElementById("menu-links")
+const mobileNavbar = document.getElementById("mobile-navbar")
+const iconMenu = document.getElementById("icon-menu")
 
-    const menu = document.getElementById("menu-title")
-    const menuLinks = document.getElementById("menu-links")
-    const mobileNavbar = document.getElementById("mobile-navbar")
-    const iconMenu = document.getElementById("icon-menu")
-    
-    menu.onclick = () => {
-      if (menuLinks.style.display === "none") {
-        menuLinks.style.display = "block"
-      } else {
-        menuLinks.style.display = "none"
-      }
-    }
-    
-    iconMenu.onclick = () => {
-      $("#mobile-navbar").slideToggle()
-      /* if (mobileNavbar.style.display === "none") {
+menu.onclick = () => {
+  if (menuLinks.style.display === "none") {
+    menuLinks.style.display = "block"
+  } else {
+    menuLinks.style.display = "none"
+  }
+}
+
+iconMenu.onclick = () => {
+  $("#mobile-navbar").slideToggle()
+  /* if (mobileNavbar.style.display === "none") {
         mobileNavbar.style.display = "block"
       } else {
         mobileNavbar.style.display = "none"
       } */
-    }
-    
-    $(window).resize(function () {
-      if (window.innerWidth <= 575) {
-        mobileNavbar.style.display = "none"
-      }
-    })
+}
 
+$(window).resize(function () {
+  if (window.innerWidth <= 575) {
+    mobileNavbar.style.display = "none"
+  }
+})
